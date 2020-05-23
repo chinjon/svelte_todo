a709216681a64a1afd87b28e554f11aba1e8a39a
Jonathan Chin <jonathankchin@gmail.com>
2020-05-23 14:56:13 -0400

    feat: init commit of component

---
+++ src/components/TodoCreate.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { todos } from './todos.js'
+  import { get } from 'svelte/store';
+</script>
+
+<form>
+  <label for="new-todo"></label>
+  <input type="text" id="new-todo" />
+  <button type="submit">Add</button>
+</form>
