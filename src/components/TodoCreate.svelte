82c9feac30b2f94922684c3a47b86869429cd84f
Jonathan Chin <jonathankchin@gmail.com>
2020-05-23 15:19:07 -0400

    feat: create add todo functionality

--- src/components/TodoCreate.svelte
+++ src/components/TodoCreate.svelte
@@ -1,10 +1,24 @@
 <script>
   import { todos } from './todos.js'
   import { get } from 'svelte/store';
+  import { createRandomNumber } from './createRandomNumber.js';
+
+  let todoList = get(todos);
+  let task = "";
+
+  const addTodo = () => {
+    todoList = [
+      ...todoList,
+      { id: createRandomNumber(1000), complete: false, task}
+    ];
+
+    todos.update(existing => todoList)
+    console.log(get(todos))
+  }
 </script>
 
-<form>
-  <label for="new-todo"></label>
-  <input type="text" id="new-todo" />
+<form on:submit|preventDefault={addTodo}>
+  <label for="new-todo">Add a todo:</label>
+  <input type="text" id="new-todo" bind:value={task} />
   <button type="submit">Add</button>
 </form>
