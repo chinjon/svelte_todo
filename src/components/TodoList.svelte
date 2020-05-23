dc663756c902247d7e1809250461d3b8233eb825
Jonathan Chin <jonathankchin@gmail.com>
2020-05-23 17:53:40 -0400

    feat: init commit of todolist component

---
+++ src/components/TodoList.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {todos} from './todos.js';
+</script>
+
+<style>
+.todo-item,
+.todo-label {
+  display: inline;
+}
+</style>
+
+<div>
+  {#each $todos as todo, i}
+    <div class="todo-wrapper">
+      <input 
+        class="todo-item" 
+        type="checkbox" 
+        id={`todo-${todo.id}`} 
+        value={todo.task} 
+        bind:checked={todo.complete}
+      />
+      <label class="todo-label" for={`todo-${i}`}>{todo.task}</label>
+    </div>
+  {/each}
+</div>
