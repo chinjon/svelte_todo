11c5cdd04349f438e5043489499158281c88e742
Jonathan Chin <jonathankchin@gmail.com>
2020-05-23 17:53:08 -0400

    chore: clean up index template

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,15 @@
 <script>
 	import TodoContainer from '../components/TodoContainer.svelte';
-	import TodoCreate from '../components/TodoCreate';
 </script>
+
 <style>
-	h1, figure {
-		text-align: center;
-		margin: 0 auto;
-	}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
 	<title>Todos</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
 <div>
 	<TodoContainer />
-	<TodoCreate />
 </div>
