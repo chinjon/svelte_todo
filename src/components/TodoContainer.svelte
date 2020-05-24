6edbb586cb06afd2e307fc6e3a8a0d64bd8ec8a7
Jonathan Chin <jonathankchin@gmail.com>
2020-05-23 22:42:38 -0400

    chore: simplify todo container

--- src/components/TodoContainer.svelte
+++ src/components/TodoContainer.svelte
@@ -1,26 +1,13 @@
 <script>
-  import {get} from 'svelte/store';
-  import {todos} from './todos.js'
+  import TodoList from './TodoList.svelte';
+  import TodoCreate from './TodoCreate.svelte';
+
+  import {getLocalStorage} from './localStorage.js';
+
+  getLocalStorage()
 </script>
 
-<style>
-.todo-item,
-.todo-label {
-  display: inline;
-}
-</style>
-
 <div>
-  {#each get(todos) as todo, i}
-    <div class="todo-wrapper">
-      <input 
-        class="todo-item" 
-        type="checkbox" 
-        id={`todo-${todo.id}`} 
-        value={todo.task} 
-        bind:checked={todo.complete}
-      />
-      <label class="todo-label" for={`todo-${i}`}>{todo.task}</label>
-    </div>
-  {/each}
+  <TodoList />
+  <TodoCreate />
 </div>
