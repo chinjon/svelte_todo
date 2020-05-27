b45b51cc30322b2aaae2cf9b106bde6715c0263b
Jonathan Chin <jonathankchin@gmail.com>
2020-05-26 21:22:31 -0400

    feat: add conditional to check if client
    purpose: to distinguish from server side rendering

--- src/components/TodoContainer.svelte
+++ src/components/TodoContainer.svelte
@@ -1,10 +1,17 @@
 <script>
-  import TodoList from './TodoList.svelte';
-  import TodoCreate from './TodoCreate.svelte';
+  import TodoList from "./TodoList.svelte";
+  import TodoCreate from "./TodoCreate.svelte";
 
-  import {getLocalStorage} from './localStorage.js';
+  import { getLocalStorage } from "./localStorage.js";
 
-  getLocalStorage()
+  if (typeof window !== "undefined") {
+    localStorage.setItem(
+      "todolist",
+      JSON.stringify([{ task: "get water", id: 1, complete: false }])
+    );
+  } else {
+    console.log("we are running on the server");
+  }
 </script>
 
 <div>
