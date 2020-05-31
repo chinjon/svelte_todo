a16e55e43c7e77652cfedc5b6f22071d01e36adb
Jonathan Chin <jonathankchin@gmail.com>
2020-05-31 13:54:35 -0400

    feat: import localstorage store

--- src/components/TodoContainer.svelte
+++ src/components/TodoContainer.svelte
@@ -1,16 +1,23 @@
 <script>
   import TodoList from "./TodoList.svelte";
   import TodoCreate from "./TodoCreate.svelte";
+  // define your stores
+  import { writable } from './localStorageStore'
+
+  export const preferences = writable('preferences', {
+    theme: 'dark',
+    pane: '50%',
+  })
 
   import { getLocalStorage, setLocalStorage } from "./localStorage.js";
   let todoListData;
 
   if (typeof window !== "undefined") {
-    todoListData = getLocalStorage("todolist");
+    todoListData = JSON.parse(getLocalStorage("todolist"));
     if (!todoListData) {
       setLocalStorage("todolist", JSON.stringify([]));
     } else {
-      todoListData = [{id: 345, task: "get water", complete: true}]
+      todoListData = JSON.parse(getLocalStorage("todolist"))
     }
   } else {
     console.log("we are running on the server");
@@ -19,5 +26,5 @@
 
 <div>
   <TodoList {todoListData} />
-  <TodoCreate />
+  <TodoCreate {todoListData}/>
 </div>
