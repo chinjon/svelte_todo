693180111ef9ef83ec353545a8fb3297c6a9db9e
Jonathan Chin <jonathankchin@gmail.com>
2020-06-01 20:16:21 -0400

    chore: remove unused code

--- src/components/TodoContainer.svelte
+++ src/components/TodoContainer.svelte
@@ -2,22 +2,13 @@
   import TodoList from "./TodoList.svelte";
   import TodoCreate from "./TodoCreate.svelte";
   // define your stores
-  import { writable } from './localStorageStore'
-
-  export const preferences = writable('preferences', {
-    theme: 'dark',
-    pane: '50%',
-  })
 
   import { getLocalStorage, setLocalStorage } from "./localStorage.js";
   let todoListData;
 
   if (typeof window !== "undefined") {
-    todoListData = JSON.parse(getLocalStorage("todolist"));
-    if (!todoListData) {
-      setLocalStorage("todolist", JSON.stringify([]));
-    } else {
-      todoListData = JSON.parse(getLocalStorage("todolist"))
+    if (getLocalStorage("todoListData")) {
+      todoListData = JSON.parse(getLocalStorage("todoListData"))
     }
   } else {
     console.log("we are running on the server");
@@ -25,6 +16,6 @@
 </script>
 
 <div>
-  <TodoList {todoListData} />
+  <TodoList {todoListData} bind:value={todoListData}/>
   <TodoCreate {todoListData}/>
 </div>
