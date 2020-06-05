aa906e3772893dffbf80aa26eb4d8d7aeb67820f
Jonathan Chin <jonathankchin@gmail.com>
2020-06-04 23:12:27 -0400

    feat: move localstorage logic to store

--- src/components/TodoContainer.svelte
+++ src/components/TodoContainer.svelte
@@ -1,21 +1,10 @@
 <script>
   import TodoList from "./TodoList.svelte";
   import TodoCreate from "./TodoCreate.svelte";
-  // define your stores
-
-  import { getLocalStorage, setLocalStorage } from "./localStorage.js";
-  let todoListData;
-
-  if (typeof window !== "undefined") {
-    if (getLocalStorage("todoListData")) {
-      todoListData = JSON.parse(getLocalStorage("todoListData"))
-    }
-  } else {
-    console.log("we are running on the server");
-  }
+  
 </script>
 
 <div>
-  <TodoList {todoListData} bind:value={todoListData}/>
-  <TodoCreate {todoListData}/>
+  <TodoList />
+  <TodoCreate />
 </div>
