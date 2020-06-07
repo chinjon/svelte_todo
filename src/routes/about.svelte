a604b66d20af5c5ad5c653dd27c9db8b2afebff0
Jonathan Chin <jonathankchin@gmail.com>
2020-06-06 21:28:28 -0400

    feat: update about page features list

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,18 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>Prototype Todo List</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<div>
+	<h2>Features Supported</h2>
+	<ul>
+		<li>Todos are saved to localstorage</li>
+	</ul>
+	<hr/>
+	<h2>Features To Add</h2>
+	<ul>
+		<li>Toggle show/hide completed</li>
+		<li>Export to JSON</li>
+		<li>Add charts - Frappe Charts</li>
+	</ul>
+</div>
