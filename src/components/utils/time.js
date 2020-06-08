a7958478466f8ccf3047b53343db22f3b6a8cacf
Jonathan Chin <jonathankchin@gmail.com>
2020-06-08 00:19:35 -0400

    doc: add comment for method

--- src/components/utils/time.js
+++ src/components/utils/time.js
@@ -1,5 +1,9 @@
 import dayjs from 'dayjs'
 
+/**
+ * @description function creates unix timestamp based 
+ * @returns {String} returns unix second 
+ */
 const createUnixStamp = () => {
   const now = dayjs().format("YYYY-MM-DD");
   return dayjs(now).unix();
