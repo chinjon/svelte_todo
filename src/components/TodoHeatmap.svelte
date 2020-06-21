47629e6eccd7f029bb5866f3d0e63e7b507bc6ea
Jonathan Chin <jonathankchin@gmail.com>
2020-06-21 11:33:54 -0400

    feat: init commit of heatmap and dashboard

---
+++ src/components/TodoHeatmap.svelte
@@ -0,0 +1,42 @@
+<script>
+  import SvelteHeatmap from 'svelte-heatmap';
+  import { Chart } from "frappe-charts"
+  const createDateStampMap = () => {
+
+  }
+
+
+function randomDate(start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).valueOf();
+}
+
+const data = {
+  dataPoints: {
+        "1578182400": 20,
+        "1580860800": 113,
+        "1587340800": 57,
+    },
+    start: new Date(2020, 0, 1), // a JS date object
+    end: new Date()
+}
+
+  setTimeout(function() {
+    try {
+    if (typeof window != 'undefined' && window.document) {
+console.log(data)
+ let chart = new Chart("#heatmap", {
+    type: 'heatmap',
+    data: data,
+})
+
+
+    }
+  } catch (e) {
+    console.log(e)
+  }
+  }, 50)
+</script>
+
+<div>
+<div id="heatmap"></div>
+</div>
---
+++ src/routes/dashboard.svelte
@@ -0,0 +1,13 @@
+<script>
+	import TodoHeatmap from '../components/TodoHeatmap.svelte';
+</script>
+
+<svelte:head>
+	<title>dashboard</title>
+</svelte:head>
+
+<h1>Dashboard</h1>
+
+<div>
+  <TodoHeatmap />
+</div>
