a4ff9252a43c3c44a032f9301fa3ef7802bc2c4f
Jonathan Chin <jonathankchin@gmail.com>
2020-06-28 11:47:52 -0400

    chore: remove outdated comments

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -52,9 +52,6 @@
 	<ul>
 		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
 		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
 		<li><a aria-current='{segment === "dashboard" ? "page" : undefined}' href='dashboard'>dashboard</a></li>
 	</ul>
 </nav>
